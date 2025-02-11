b58fbe511ed859fff17394608421167420808ec7
cdalinghaus <108818676+cdalinghaus@users.noreply.github.com>
2025-02-11 11:50:24 +0100

    Update vector_helpers.py

--- hela_datasets/vector_helpers.py
+++ hela_datasets/vector_helpers.py
@@ -131,8 +131,10 @@ def get_scaled_centroid_map(mask_img, scalars, centroid_size_sigma):
     a = np.array(gaussiansx)
     b = np.array(gaussiansy)
     # Find the most extreme value
-    print(a.shape)
-    a_extreme = a[np.argmax(np.abs(a), axis=0)]
+    print("a", a.shape)
+    a_max_indices = np.argmax(np.abs(a), axis=0)
+    print("a_max_indices", a_max_indices.shape)
+    a_extreme = a[a_max_indices]
     print(a_extreme.shape)
     b_extreme = b[np.argmax(np.abs(b), axis=0)]
     return a_extreme, b_extreme
